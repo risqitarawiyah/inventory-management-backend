184fa16458b89c6b2a6363977a408cc647b6f40d
risqitarawiyah <152830283+risqitarawiyah@users.noreply.github.com>
2024-09-27 11:12:05 +0700

    telah menambahkan file

---
+++ src/app.js
@@ -0,0 +1,16 @@
+const express = require('express')
+const app = express()
+const dotenv = require('dotenv')
+dotenv.config();
+
+const PORT = process.env.PORT;
+
+app.use(express.json());
+
+app.get("/", (req, res) => {
+    res.send("Hello There!")
+})
+
+app.listen(PORT, () => {
+    console.log('App listening on port' + PORT)
+})
