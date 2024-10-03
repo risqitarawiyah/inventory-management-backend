80d0c41553fe3d85c6f49e6b51fe3c1a2092af8a
risqitarawiyah <152830283+risqitarawiyah@users.noreply.github.com>
2024-10-03 10:34:30 +0700

    Menambahkan folder baru dan memodifikasi src/app.js

--- src/app.js
+++ src/app.js
@@ -11,6 +11,9 @@ app.get("/", (req, res) => {
     res.send("Hello There!")
 })
 
+const authController = require("./auth/auth.controller");
+app.use("/api/auth", authController);
+
 app.listen(PORT, () => {
     console.log('App listening on port' + PORT)
 })
