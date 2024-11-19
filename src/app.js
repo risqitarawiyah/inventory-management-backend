3de5974369f5a023e7e4bb120c2cf1729aba63cf
risqitarawiyah <152830283+risqitarawiyah@users.noreply.github.com>
2024-11-19 14:26:08 +0700

    modifikasi file app.js

--- src/app.js
+++ src/app.js
@@ -1,12 +1,14 @@
-const express = require('express')
-const app = express()
-const dotenv = require('dotenv')
+const express = require('express');
+const cors = require('cors');
+const app = express();
+const dotenv = require('dotenv');
 dotenv.config();
-const adminAuthorization = require('./middleware/adminAuthorization')
+const adminAuthorization = require('./middleware/adminAuthorization');
 
 const PORT = process.env.PORT;
 
 app.use(express.json());
+app.use(cors());
 
 app.get("/", (req, res) => {
     res.send("Hello There!")
@@ -19,7 +21,6 @@ const transactionController = require("./transaction/transaction.controller");
 
 app.use("/api/auth", authController);
 app.use("/api/items", itemController);
-app.use("/api/users", userController);
 app.use("/api/users", adminAuthorization, userController);
 app.use("/api/transactions", transactionController);
 
